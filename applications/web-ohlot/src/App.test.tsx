d5ccf3c8562a54f2f80afb24bdd186f1f92bd95c
Jihyun Lee <jihyunlee.me@gmail.com>
2022-03-27 22:26:23 +0900

    [web] 불필요한 파일 제거

--- applications/web-ohlot/src/App.test.tsx
+++ applications/web-ohlot/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import GoodWordPage from './pages/goodWord/GoodWordPage';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<GoodWordPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
