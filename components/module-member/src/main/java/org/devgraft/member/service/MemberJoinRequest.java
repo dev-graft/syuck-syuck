9eede98d4fac64ef1452f56435e3d4dfecd598d9
PCloud <pcloud63514@gmail.com>
2022-04-12 21:19:25 +0900

    Update

--- components/module-member/src/main/java/org/devgraft/member/service/MemberJoinRequest.java
+++ components/module-member/src/main/java/org/devgraft/member/service/MemberJoinRequest.java
@@ -4,12 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * 정규식 특수문자
+ * / 정규식 시작
+ * ^ 문자열 시작
+ * [] 범위
+ * A-Z A~Z
+ * | or
+ * + 앞의 조건이 한번 이상 호출되었는지
+ * $ 문자열 끝
+ * / 정규식 끝
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class MemberJoinRequest {
+    // 한, 영(대소문자), 숫자 혀용
+    @Pattern(regexp = "/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/")
+    @Size(min = 2, max = 10)
     private String nickName;
+    // 영(소/대) 숫자 허용
+    @Pattern(regexp = "/^[A-Z|a-z|0-9]+$/")
+    @Size(min = 6, max = 20)
     private String id;
+    // 영(소/대) / 특수문자, 숫자 허용
+    @Size(min = 8, max = 20)
     private String password;
     private int gender;
 }
