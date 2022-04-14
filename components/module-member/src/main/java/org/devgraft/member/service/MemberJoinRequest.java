e4f5a3d89f5324d09f91c357fb76cbf4493fffe5
PCloud <pcloud63514@gmail.com>
2022-04-14 17:53:05 +0900

    Module-Member Update Gender Type 변경 int -> GenderEnum

--- components/module-member/src/main/java/org/devgraft/member/service/MemberJoinRequest.java
+++ components/module-member/src/main/java/org/devgraft/member/service/MemberJoinRequest.java
@@ -3,7 +3,11 @@ package org.devgraft.member.service;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.devgraft.member.domain.GenderEnum;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 /**
@@ -28,5 +32,6 @@ public class MemberJoinRequest {
     private String id;
     @Pattern(regexp = "^(?=^.{8,20}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*+=.,\\\\\\-<>/;:'~₩]).*$")
     private String password;
-    private int gender;
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
 }
