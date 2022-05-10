bbc10a377c5339e82704555226aa3271ad476435
PCloud <pcloud63514@gmail.com>
2022-05-10 11:47:57 +0900

    Spring Cloud Gateway에서 Swagger 합병 구현

---
+++ components/module/gateway/src/main/java/org/devgraft/gateway/config/handler/SwaggerResourceHandler.java
@@ -0,0 +1,31 @@
+package org.devgraft.gateway.config.handler;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Component
+@AllArgsConstructor
+public class SwaggerResourceHandler implements HandlerFunction {
+    private final SwaggerResourcesProvider swaggerResources;
+
+    /**
+     * Handle the given request.
+     *
+     * @param request the request to handler
+     * @return the response
+     */
+    @Override
+    public Mono handle(ServerRequest request) {
+        return ServerResponse.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(BodyInserters.fromObject(swaggerResources.get()));
+    }
+}
