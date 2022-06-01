ded74a313222cdcc31ed2f8505b0edd6d81f0daf
PCloud <pcloud63514@gmail.com>
2022-06-01 20:57:21 +0900

    reset

--- components/support/advice-redis/src/test/java/org/devgraft/adviceredis/StubUUIDProvider.java
+++
@@ -1,6 +0,0 @@
-package org.devgraft.adviceredis;
-
-import com.dreamsecurity.simple.provider.UUIDProvider;
-
-public class StubUUIDProvider extends UUIDProvider {
-}
--- components/support/auth-store/src/test/java/org/devgraft/authstore/SpyAuthUtil.java
+++
@@ -1,6 +0,0 @@
-package org.devgraft.authstore;
-
-import com.dreamsecurity.auth.util.AuthUtil;
-
-public class SpyAuthUtil extends AuthUtil {
-}
