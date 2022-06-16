0ac13735d977ed31827ef1dbe2518be5ab6a324b
PCloud <pcloud63514@gmail.com>
2022-06-16 14:53:06 +0900

    mui 추가

---
+++ applications/web-chat/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material'
+export default function Home() {
+    return (
+        <div>
+            <Button variant={'outlined'}>cancel</Button>
+            <Button variant={'contained'}>submit</Button>
+        </div>
+    )
+}
