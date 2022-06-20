cdcfaceb6398969ad31ddf511809fe5c4a4662be
PCloud <pcloud63514@gmail.com>
2022-06-20 13:06:32 +0900

    프로젝트 추가

--- components/module-chat/src/main/java/org/devgraft/module/chat/oauth/provider/OAuthAttributeProvider.java
+++ components/module-chat/src/main/java/org/devgraft/module/chat/oauth/provider/OAuthAttributeProvider.java
@@ -8,12 +8,12 @@ import java.util.Map;
 @Component
 public class OAuthAttributeProvider {
 
-    OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+    public OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
         return ofGoogle(userNameAttributeName, attributes).build();
     }
 
 
-    OAuthAttributes.OAuthAttributesBuilder ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+    public OAuthAttributes.OAuthAttributesBuilder ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
