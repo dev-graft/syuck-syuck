637184ca8715cea94276a7278d8b1ce999fdb816
PCloud <pcloud63514@gmail.com>
2022-06-26 03:22:12 +0900

    Member email 속성 추가

--- components/member/src/main/java/org/devgraft/member/Member.java
+++ components/member/src/main/java/org/devgraft/member/Member.java
@@ -18,6 +18,8 @@ import javax.persistence.Id;
 public class Member extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "email", unique = true)
+    private String email;
     @Column(name = "profile_image")
     private String profileImage;
     @Column(name = "nick_name")
@@ -27,17 +29,19 @@ public class Member extends BaseEntity {
     @Column(name = "state_message")
     private String stateMessage;
 
-    private Member(Long id, String profileImage, String nickName, String identifyToken, String stateMessage) {
+    private Member(Long id, String email, String profileImage, String nickName, String identifyToken, String stateMessage) {
         this.id = id;
+        this.email = email;
         this.profileImage = profileImage;
         this.nickName = nickName;
         this.identifyToken = identifyToken;
         this.stateMessage = stateMessage;
     }
 
-    public static Member of(final String profileImage, final String nickName, final String identifyToken, final String stateMessage) {
+    public static Member of(final String email, final String profileImage, final String nickName, final String identifyToken, final String stateMessage) {
+        Assert.notNull(email, "email must not be null");
         Assert.notNull(nickName, "nickName must not be null");
         Assert.notNull(identifyToken, "identifyToken must not be null");
-        return new Member(null, profileImage, nickName, identifyToken, stateMessage);
+        return new Member(null, email, profileImage, nickName, identifyToken, stateMessage);
     }
 }
--- components/member/src/main/java/org/devgraft/member/MemberJoinRequest.java
+++ components/member/src/main/java/org/devgraft/member/MemberJoinRequest.java
@@ -5,17 +5,24 @@ import org.springframework.util.Assert;
 
 @Getter
 public class MemberJoinRequest {
+    private final String email;
     private final String profileImage;
     private final String nickName;
     private final String identifyToken;
     private final String stateMessage;
 
-    public MemberJoinRequest(String profileImage, String nickName, String identifyToken, String stateMessage) {
+    private MemberJoinRequest(String email, String profileImage, String nickName, String identifyToken, String stateMessage) {
+        Assert.notNull(email, "email must not be null");
         Assert.notNull(nickName, "nickName must not be null");
         Assert.notNull(identifyToken, "identifyToken must not be null");
+        this.email = email;
         this.profileImage = profileImage;
         this.nickName = nickName;
         this.identifyToken = identifyToken;
         this.stateMessage = stateMessage;
+    }
+
+    public static MemberJoinRequest of(final String email, final String profileImage, final String nickName, final String identifyToken, final String stateMessage) {
+        return new MemberJoinRequest(email, profileImage, nickName, identifyToken, stateMessage);
     }
 }
