591d2de66692beca2e5381eae109158ff22d1a82
PCloud <pcloud63514@gmail.com>
2022-06-26 04:46:36 +0900

    OAuth 및 Member 가입 로직 적용 완료

--- components/module-auth/src/main/java/org/devgraft/auth/CustomOAuth2UserServiceImpl.java
+++ components/module-auth/src/main/java/org/devgraft/auth/CustomOAuth2UserServiceImpl.java
@@ -2,13 +2,18 @@ package org.devgraft.auth;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.devgraft.member.MemberJoinRequest;
 import org.devgraft.member.MemberService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 
 @Slf4j
@@ -21,15 +26,17 @@ public class CustomOAuth2UserServiceImpl implements CustomOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
-
         // google, naver, kakao...
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
-        // member에서 조회
-        // 이미 있으면 냅두고
-        // 없으면 맴버에 추가
+        OAuthAttributes oa = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        if (!memberService.alreadyJoin(oa.getName())) {
+            memberService.join(MemberJoinRequest.of(oa.getEmail(), oa.getPicture(), oa.getName(), oAuth2User.getName(), null));
+        }
 
-        return oAuth2User;
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+                oa.getAttributes(),
+                oa.getNameAttributeKey());
     }
 }
--- components/module-auth/src/main/java/org/devgraft/auth/OAuth2AuthenticationSuccessHandler.java
+++ components/module-auth/src/main/java/org/devgraft/auth/OAuth2AuthenticationSuccessHandler.java
@@ -2,8 +2,10 @@ package org.devgraft.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.devgraft.member.MemberService;
+import org.devgraft.support.jwt.JwtGenerateRequest;
 import org.devgraft.support.jwt.JwtService;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -21,9 +23,11 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
-        // accessToken
-        // refreshToken
-        super.onAuthenticationSuccess(request, response, authentication);
+        OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
+        Long memberId = memberService.getMemberId(oAuth2User.getName());
+        String accessToken = jwtService.generateToken(JwtGenerateRequest.of(memberId.toString(), "ROLE_USER", 600));
+        String refreshToken = jwtService.generateToken(JwtGenerateRequest.of(accessToken, "ROLE_USER", 3600 * 30 * 12));
+        authUtil.injectAuthorization(accessToken, refreshToken, response);
+        response.sendRedirect("/home");
     }
 }
