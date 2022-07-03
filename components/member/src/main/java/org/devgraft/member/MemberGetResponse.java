20623694ce3bdb26eeeb582c30c5aa2198219ece
PCloud <pcloud63514@gmail.com>
2022-07-04 01:35:47 +0900

    Update

--- components/member/src/main/java/org/devgraft/member/MemberGetResponse.java
+++ components/member/src/main/java/org/devgraft/member/MemberGetResponse.java
@@ -1,13 +1,22 @@
 package org.devgraft.member;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.Assert;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class MemberGetResponse {
     private final String email;
     private final String nickName;
     private final String profileImage;
     private final String stateMessage;
+
+    public static MemberGetResponse of(final String email, final String nickName, final String profileImage, final String stateMessage) {
+        Assert.notNull(email, "email must not be null");
+        Assert.notNull(nickName, "nickName must not be null");
+        Assert.notNull(profileImage, "profileImage must not be null");
+        return new MemberGetResponse(email, nickName, profileImage, stateMessage);
+    }
 }
---
+++ components/module-member/src/test/java/org/devgraft/member/MemberApiTest.java
@@ -0,0 +1,49 @@
+package org.devgraft.member;
+
+import org.devgraft.auth.AuthResult;
+import org.devgraft.auth.CredentialsResolver;
+import org.devgraft.auth.MemberCredentials;
+import org.devgraft.auth.SpyAuthService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class MemberApiTest {
+    private SpyMemberService spyMemberService;
+    private SpyAuthService spyAuthService;
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        spyMemberService = new SpyMemberService();
+        spyAuthService = new SpyAuthService();
+        mockMvc = MockMvcBuilders.standaloneSetup(new MemberApi(spyMemberService))
+                .setCustomArgumentResolvers(new CredentialsResolver(spyAuthService))
+                .build();
+    }
+
+    @DisplayName("자신의 프로필 정보 갖고오기")
+    @Test
+    void getMyProfile_Api_Test() throws Exception {
+        spyAuthService.exportAuthorization_returnValue = Optional.of(AuthResult.of("access", "refresh"));
+        spyAuthService.verity_returnValue = MemberCredentials.of(2L, "USER");
+        spyMemberService.getMember_returnValue = MemberGetResponse.of("email", "nickName", "profileImage", "stateMessage");
+
+        mockMvc.perform(get("/api/v1/members/me"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email", equalTo("email")))
+                .andExpect(jsonPath("$.nickName", equalTo("nickName")))
+                .andExpect(jsonPath("$.profileImage", equalTo("profileImage")))
+                .andExpect(jsonPath("$.stateMessage", equalTo("stateMessage")))
+        ;
+    }
+}
