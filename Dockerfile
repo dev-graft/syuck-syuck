9320e026949b8a605f78c32185c264ef6d21e68a
PCloud <pcloud63514@gmail.com>
2022-07-06 23:46:21 +0900

    CD 테스트

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM openjdk:11-jre-slim
-ARG PROFILE=default
-ENV profile=${PROFILE}
+ENV PROFILE default
 RUN apt-get update && apt-get install -y fontconfig libfreetype6 && apt-get install -y tzdata
 ENV TZ Asia/Seoul
 COPY applications/app-demo/build/libs/*.jar app.jar
-ENTRYPOINT java -jar -Dspring.profiles.active=$profile app.jar
+ENTRYPOINT java -jar -Dspring.profiles.active=$PROFILE app.jar
--- components/module-auth/src/main/java/org/devgraft/auth/AuthController.java
+++ components/module-auth/src/main/java/org/devgraft/auth/AuthController.java
@@ -2,6 +2,7 @@ package org.devgraft.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +17,11 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("success")
-    public void authSuccess(@RequestParam String token, HttpServletResponse response, HttpSession session) {
+    public String authSuccess(@RequestParam(name = "token") String token, HttpServletResponse response, HttpSession session, Model model) {
         String refresh = (String)session.getAttribute("refresh");
         session.removeAttribute("refresh");
         authService.injectAuthorization(token, refresh, response);
+        model.addAttribute(TokenName.ACCESS_TOKEN_SYNTAX, token);
+        return "token";
     }
 }
